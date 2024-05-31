460f79299a70dff71298e63002abada449794057
eundonghyeok <mephisto7191@naver.com>
2024-05-31 23:33:46 +0900

    mockdata 삭제 및 fetch로 네트워크 데이터 가져오기

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import ReviewList from "./ReviewList";
-import mockItems from "../mock.json";
 import { useState } from "react";
+import ReviewList from "./ReviewList";
+import { getReviews } from "../api";
 
 export default function App() {
-  const [items, setItems] = useState(mockItems);
   const [order, setOrder] = useState("createdAt");
+
+  const [items, setItems] = useState([]);
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
@@ -16,6 +18,11 @@ export default function App() {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews);
+  };
+
   return (
     <div>
       <div>
@@ -23,6 +30,7 @@ export default function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
