56d17a2f596b4a3f0e67f3f10fbbc8154da9cccc
eundonghyeok <mephisto7191@naver.com>
2024-05-31 21:54:49 +0900

    mock 데이터 추가 및 map메서드로 배열 렌더링

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,4 +1,33 @@
+import "./ReviewList.css";
+
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
+function ReviewListItem({ item }) {
+  return (
+    <div className="ReviewListItem">
+      <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
+      <div>
+        <h1>{item.title}</h1>
+        <p>{item.rating}</p>
+        <p>{formatDate(item.createdAt)}</p>
+        <p>{item.content}</p>
+      </div>
+    </div>
+  );
+}
+
 export default function ReviewList({ items }) {
   console.log(items);
-  return <ul></ul>;
+  return (
+    <ul>
+      {items.map((item) => (
+        <li>
+          <ReviewListItem item={item} />
+        </li>
+      ))}
+    </ul>
+  );
 }
