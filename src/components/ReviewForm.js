7434f81eb36319a597f5d2e7ee493008eb975ccf
eundonghyeok <mephisto7191@naver.com>
2024-06-01 16:56:37 +0900

    별점 인풋 만들기

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import FileInput from "./FileInput";
+import RatingInput from "./RatingInput";
 import "./ReviewForm.css";
 
 export default function ReviewForm() {
@@ -34,11 +35,11 @@ export default function ReviewForm() {
         onChange={handleChange}
       />
       <input name="title" value={values.title} onChange={handleInputChange} />
-      <input
+      <RatingInput
         name="rating"
         type="number"
         value={values.rating}
-        onChange={handleInputChange}
+        onChange={handleChange}
       />
       <textarea
         name="content"
