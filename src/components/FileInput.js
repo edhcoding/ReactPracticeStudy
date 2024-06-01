697de919dcf89fc5ff051c474b35860b38836892
eundonghyeok <mephisto7191@naver.com>
2024-06-01 16:22:57 +0900

    URL.revokeObjectURL 사이드 이펙트 정리하기

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -18,14 +18,26 @@ export default function FileInput({ name, value, onChange }) {
 
   useEffect(() => {
     if (!value) return;
-    setPreview(URL.createObjectURL(value));
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
     // objectURL을 만들면 웹 브라우저는 메모리를 할당하고 파일에 해당하는 주소를 만들어 줌
+    // objectURL이 만든 메모리가 바로 - 사이드 이펙트
+
+    //useEffect는 사이드 이펙트를 만들고 정리하는 방법도 제공함 return값으로 함수를 리터하면 됨
+    return () => {
+      setPreview();
+      // 초기화 해주고
+      URL.revokeObjectURL(nextPreview);
+      // revokeObjextURL - 사이드 이펙트를 정리하는 코드
+      // 앞에서 만든 ObjectURL 해제 - 메모리 계속 할당해주는거 막음
+    };
   }, [value]);
 
   return (
     <div>
       <img src={preview} alt="이미지 미리보기" />
-      <input type="file" onChange={handleChange} ref={inputRef} />
+      <input type="file" accept="image/png, image/jpeg" onChange={handleChange} ref={inputRef} />
+      {/* 파일인풋을 이미지 파일 하나만 선택할때 사용할 거기 때문에 accept 속성 */}
       {value && <button onClick={handleClearClick}>X</button>}
     </div>
   );
