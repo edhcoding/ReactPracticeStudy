533017a8755d153b7dd4600e3cd0e2497bae0f74
eundonghyeok <mephisto7191@naver.com>
2024-06-01 15:52:30 +0900

    파일 인풋 초기화

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,9 +1,29 @@
+import { useRef } from "react";
+
 export default function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     onChange(name, e.target.files[0]);
   };
 
-  return <input type="file" onChange={handleChange} />;
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+    inputNode.value = "";
+    onChange(name, null);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef} />
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
+  );
   // file 타입은 value를 props로 지정 불가 - 해커가 개인정보를 가져갈 수도 있음
+  // 따라서 fileInput의 value속성은 사용자만 직접 변경가능
+  // 자바스크립트로 바꿀때는 빈문자열로만 변경가능
   // fakepath
+  // DOM 노드는 반드시 렌더링이 끝나야 생성이 되니까 ref객체의 current값도 화면에 컴포넌트가 렌더링 되었을때만 존재함
+  // 그래서 항상 if (inputRef.current){}로 값이 존재하는지 확인하고 사용하는 걸 추천함
 }
