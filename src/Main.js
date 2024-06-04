c1e1890a8f162896195272219f3d9f8ccb5f2220
eundonghyeok <mephisto7191@naver.com>
2024-06-04 14:59:12 +0900

    하위 페이지 나누기 (index, outlet)

--- src/Main.js
+++ src/Main.js
@@ -8,17 +8,18 @@ import WishlistPage from "./pages/WishlistPage";
 function Main() {
   return (
     <BrowserRouter>
-      <App>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="courses" element={<CourseListPage />} />
-          <Route
-            path="courses/react-frontend-development"
-            element={<CoursePage />}
-          />
+      <Routes>
+        <Route path="/" element={<App />}>
+          {/* 하위 컴포넌트에 공통된 디자인을 보여주고 싶을때는 Route안에 element 프롭에 App 컴포넌트를 사용하고
+          App 컴포넌트 안에 Outlet 컴포넌트를 넣어주면 됨 */}
+          <Route index element={<HomePage />} />
+          <Route path="courses">
+            <Route index element={<CourseListPage />} />
+            <Route path="react-frontend-development" element={<CoursePage />} />
+          </Route>
           <Route path="wishlist" element={<WishlistPage />} />
-        </Routes>
-      </App>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
